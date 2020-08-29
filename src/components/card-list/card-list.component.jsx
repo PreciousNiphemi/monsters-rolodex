84d9d0ec78a1b8a5c476bdcc51a3fff00aaf1f2c
PreciousNiphemi <preciousoluwanifemi1999@gmail.com>
2020-08-29 16:18:09 -0700

    mehn first time deploying with react

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Card} from '../card/card.component';
+
+import './card-list.styles.css';
+
+export const CardList = (props) =>{
+
+return ( <div className='card-list'>
+    {
+            props.monsters.map(monster=>{
+            return (<Card key={monster.id} monster={monster}/>)
+            })
+
+          }
+
+</div>);
+}
